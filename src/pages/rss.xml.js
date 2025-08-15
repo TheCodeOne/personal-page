3200fad9b21cfc25a2caff51d09e92c54f712e9d
Dimitrios Kokkonias <dimi.kokkonias@gmail.com>
2025-08-15 15:17:42 +0200

    🎨 Format codebase

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -5,13 +5,9 @@ import { getCollection } from "astro:content";
 export async function GET(context) {
   const blog = (await getCollection("blog")).filter((post) => !post.data.draft);
 
-  const projects = (await getCollection("projects")).filter(
-    (project) => !project.data.draft,
-  );
+  const projects = (await getCollection("projects")).filter((project) => !project.data.draft);
 
-  const items = [...blog, ...projects].sort(
-    (a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf(),
-  );
+  const items = [...blog, ...projects].sort((a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf());
 
   return rss({
     title: SITE.TITLE,
