8ac3456334f5c4bff315adae0d54f2a4551bdcc4
Dimitrios Kokkonias <dimi.kokkonias@gmail.com>
2025-08-18 13:24:21 +0200

    🙈 Adjust mail

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,7 @@ import type { Metadata, Site, Socials } from "@types";
 export const SITE: Site = {
   TITLE: "Aeh...",
   DESCRIPTION: "Ein paar meiner Gedanken.",
-  EMAIL: "me@dimi.cool",
+  EMAIL: "www.@dimi.cool",
   NUM_POSTS_ON_HOMEPAGE: 5,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
