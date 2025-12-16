1a4ac1354f8661c8903b759fec1cecd1459159bc
Dimitrios Kokkonias <dimi.kokkonias@gmail.com>
2025-12-16 11:13:17 +0100

    ✨ Add support for latex math

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,26 @@ import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import pagefind from "astro-pagefind";
 import { defineConfig } from "astro/config";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://dimi.cool",
-  integrations: [sitemap(), mdx(), pagefind()],
+  integrations: [
+    sitemap(),
+    mdx({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
+    pagefind(),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
   markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
     shikiConfig: {
       theme: "css-variables",
     },
